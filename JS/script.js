8c0d6e68245df66c0ea52362291b98d4ec17b405
himanshu864 <himanshu864000@gmail.com>
2024-05-15 23:16:01 +0530

    Email overlap some javascript no responsive or 2nd

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,12 @@
-console.log("Make FAQ Expend")
+let emailInput = document.getElementById("email-address1");
+let emailBox = document.querySelector(".email-holder");
+
+emailInput.addEventListener("focusin", () => {
+    emailBox.style.fontSize = "13px";
+    emailBox.style.transform = "translateY(-10px)"
+})
+
+emailInput.addEventListener("focusout", () => {
+    emailBox.style.transform = "translateY(0)"
+    emailBox.style.fontSize = "18px";
+})
